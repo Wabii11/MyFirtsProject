772eebb7dd0dacca7ce4b90b73f5c730a665c4d7
wendimu mulata <wabii.dinsa@gmail.com>
2023-05-29 22:54:54 -0500

    my first java project

---
+++ src/JavaPractice.java
@@ -0,0 +1,12 @@
+
+public class JavaPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number=10;
+			System.out.println("this is a number"+ number);
+		
+
+	}
+
+}
